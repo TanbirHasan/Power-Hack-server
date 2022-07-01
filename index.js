d087cba610e2e3116e2731f68515b21683a1bf60
unknown <mdtanbir576@gmail.com>
2022-07-01 15:49:31 +0600

    updating information

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 7000;
 
 app.use(express.json());
@@ -38,6 +38,7 @@ async function run(){
 
       app.post("/api/add-billing", async (req, res) => {
         const newbilling = req.body;
+        console.log(newbilling)
         const result = await billingcollection.insertOne(newbilling);
         res.send(result);
       });
@@ -45,11 +46,72 @@ async function run(){
       // getting all billing
 
       app.get("/api/billing-list", async (req, res) => {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
         const query = {};
-        const cursor = billingcollection.find(query);
-        const billing = await cursor.toArray();
+        const cursor = billingcollection.find(query).sort({_id:-1});
+        let billings;
+        if (page || size) {
+          billing = await cursor
+            .skip(page * size)
+            .limit(size)
+            .toArray();
+        } else {
+          billing = await cursor.toArray();
+        }
+
         res.send(billing);
       });
+
+      // getting billings number for pagination
+
+      app.get("/api/billings-count", async (req, res) => {
+        const query = {};
+        const cursor = billingcollection.find(query);
+        const count = await cursor.count();
+        res.send({ count });
+      });
+
+      // Deleting the billing list
+      app.delete("/api/delete-billing/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = billingcollection.deleteOne(query);
+        res.send(result);
+      });
+
+
+
+    //  app.put("/api/update-billing/:id", async (req, res) => {
+    //     const id = req.params.id;
+    //    const userInfo = req.body;
+     
+     
+      
+    //    const result = await billingcollection.updateOne(
+    //      { _id:  ObjectId(id) },
+    //      { $set: { fullname:req.body.fullname,email:req.body.email,phone:req.body.phone,paidamount:req.body.paidamount } },
+    //      { upsert: true }
+    //    );
+
+    //    res.send(result);
+    //  });
+
+     // search result
+
+     app.get("/api/billings/search", async (req,res) => {
+          try {
+            const searchParams = req.query;
+            console.log(searchParams);
+            const result = await billingcollection.find(searchParams);
+            if (!result) throw Error("Error, No result Found...!");
+            res.status(200).json(result);
+          } catch (err) {
+            res.status(400).json({ msg: err });
+          }
+     })
+
+
     }
     finally{
 
